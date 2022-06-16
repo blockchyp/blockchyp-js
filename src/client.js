c6d762ce9951dc2e05eca8a36789228766edee0f
BlockChyp SDK Builder <devops@blockchyp.com>
2022-06-16 19:38:12 +0000

    Merge pull request #207 from blockchyp/feature/CHYP-2705
    added update-merchant to CLI and fixed CLI example

--- src/client.js
+++ src/client.js
@@ -410,7 +410,7 @@ class BlockChypClient {
 
   /**
    * Adds or updates a merchant account. Can be used to create or update test merchants.
-   * Only gateway only partners may create new live merchants.
+   * Only gateway partners may create new live merchants.
    */
   updateMerchant (request) {
     return this._dashboardRequest('post', '/api/update-merchant', request)
