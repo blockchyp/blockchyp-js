e0ae0ce12fb28ce5ae513e97054f3f26d0efee2f
BlockChyp SDK Builder <devops@blockchyp.com>
2023-10-30 10:39:24 -0600

    Merge pull request #253 from blockchyp/develop
    Develop

---
+++ itest/MerchantInvoiceDetailITestSpec.js
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2019-2023 BlockChyp, Inc. All rights reserved. Use of this code is governed
+ * by a license that can be found in the LICENSE file.
+ *
+ * This file was generated automatically by the BlockChyp SDK Generator. Changes to this
+ * file will be lost every time the code is regenerated.
+ */
+
+describe('MerchantInvoiceDetail', function () {
+  var uuidv4 = require('uuid/v4');
+  var fs = require('fs');
+  var Config = require('../itest/support/config').config;
+  Config.load();
+  var BlockChyp = require('../index.js');
+  var lastTransactionId, lastTransactionRef, lastCustomerId, lastToken, uploadId;
+
+  beforeEach(function () {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+  });
+
+  it('can retrieve merchant invoice detail', function (done) {
+    var client = BlockChyp.newClient(Config.getCreds())
+    client.setGatewayHost(Config.getGatewayHost())
+    client.setTestGatewayHost(Config.getTestGatewayHost())
+    client.setDashboardHost(Config.getDashboardHost())
+
+    var testDelay = process.env.BC_TEST_DELAY
+    var testDelayInt = 0
+    if (testDelay) {
+      testDelayInt = parseInt(testDelay)
+    }
+
+
+    testDelay = 0
+
+
+    if (testDelay > 0) {
+      var messageRequest = {
+        test: true,
+        terminalName: Config.getTerminalName(),
+        message: 'Running MerchantInvoiceDetail in ' + testDelay + ' seconds...'
+      }
+      client.message(messageRequest)
+        .then(function (httpResponse) {
+          let response = httpResponse.data
+          expect(response.success).toBe(true)
+        })
+        .catch(function (error) {
+          console.log('Error:', error)
+          done()
+        })
+    }
+
+    console.log('Running merchantInvoiceDetail...')
+
+    setTimeout(function () {
+      client = BlockChyp.newClient(Config.getCreds(''))
+      client.setGatewayHost(Config.getGatewayHost())
+      client.setTestGatewayHost(Config.getTestGatewayHost())
+      client.setDashboardHost(Config.getDashboardHost())
+      // setup request object
+      let request = {
+        test: true,
+      }
+
+     client.merchantInvoiceDetail(request)
+      .then(function (httpResponse) {
+          let response = httpResponse.data
+          // console.log('TEST RESPONSE:' + JSON.stringify(response))
+
+          // response assertions
+          expect(response.success).toBe(true)
+          done()
+        })
+        .catch(function (error) {
+          console.log('Error:', error)
+          fail(error)
+          done()
+        })
+    }, testDelayInt * 1000);
+  });
+});
