7da9bd3217f87dce541cd9503e415769c638996c
BlockChyp SDK Builder <devops@blockchyp.com>
2024-09-17 19:41:08 +0000

    Merge pull request #282 from blockchyp/feature/CHYP-3620
    Added merchant application submission

--- examples/SubmitApplicationExample.js
+++ examples/SubmitApplicationExample.js
@@ -7,8 +7,7 @@ let client = BlockChyp.newClient({
   signingKey: '9c6a5e8e763df1c9256e3d72bd7f53dfbd07312938131c75b3bfd254da787947'
 });
 
-client.merchantCredentialGeneration({
-  merchantId: '<MERCHANT ID>',
+client.submitApplication({
 })
 .then(function (response) {
     console.log('Response: ' + JSON.stringify(response.data))
